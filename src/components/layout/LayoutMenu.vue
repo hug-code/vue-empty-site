03dceb7b52f3992959e179dc8e5458e8645af063
DESKTOP-M48INUK\injurys <1762910894@qq.com>
2020-12-18 10:35:10 +0800

    处理菜单折叠异常

--- src/components/layout/LayoutMenu.vue
+++ src/components/layout/LayoutMenu.vue
@@ -1,14 +1,12 @@
 <template>
     <a-menu
-            :open-keys="openKeys"
-            :default-selected-keys="[$route.name]"
-            :selected-keys="openKeys"
-            :mode="menuMode"
-            :theme="sideTheme"
-            :inline-collapsed="sideCollapsed"
-            :class="sideCollapsed ? 'ant-menu-inline-collapsed' : ''"
-            @openChange="onOpenChange"
-            @click="onClick"
+        :open-keys="openKeys"
+        :selected-keys="openKeys"
+        :mode="menuMode"
+        :theme="sideTheme"
+        :class="sideCollapsed ? 'ant-menu-inline-collapsed' : ''"
+        @openChange="onOpenChange"
+        @click="onClick"
     >
         <template v-for="item in menuList">
             <a-menu-item v-if="!item.children" :key="item.key">
@@ -75,7 +73,12 @@
             selectMenu() { // 生成展开及选中的菜单
                 setTimeout(() => {
                     let matched = this.$route.matched
-                    this.openKeys = this.openKeysTemp = matched.map(item => item.name)
+                    this.openKeysTemp = matched.map(item => item.name)
+                    if(this.sideCollapsed) {
+                        this.menuMode = 'vertical'
+                    }else{
+                        this.openKeys = this.openKeysTemp
+                    }
                 }, 10)
             }
         },
